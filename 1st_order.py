94f6280849bc9cea753bd414f9492fe4c5929f20
lukas-sr <lukas-sr@hotmail.com>
2021-04-28 10:40:10 -0300

    adding 1st order dynamic simulation

---
+++ 1st_order.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# K is the process Gain and T is the time constant
+K = 3
+T = 4
+start = 0
+stop = 30
+increment = 0.1
+# array for the x axis
+time = np.arange(start,stop,increment) 
+y = K*(1-np.exp(time/T))
+plt.plot(time,y)
+plt.title("1st Order Dynamic System")
+plt.xlabel("t[s]")
+plt.ylabel("y(s)")
+plt.grid()
