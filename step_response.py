c4bf70d808a67316e2deb6b2240572282bcd45fe
lukas-sr <lukas-sr@hotmail.com>
2021-04-30 11:42:55 -0300

    adding step response code

---
+++ step_response.py
@@ -0,0 +1,21 @@
+import control as co
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+
+
+# H(s) = 3 / 4s + 1
+
+num = np.array([3])
+den = np.array([4 , 1])
+
+H = co.tf(num, den)
+print('H(s) = '+ str(H))
+
+t , y_s = co.step_response(H)
+
+simulation = go.Figure(data=go.Scatter(x=t, y=y_s))
+simulation.update_layout(title = "Step Response",
+        xaxis_title = "time [s]",
+        yaxis_title = "y(s)")
+simulation.show()
