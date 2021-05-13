b5197f9340e242263e631d50c94f0e9534854845
lukas-sr <lukas-sr@hotmail.com>
2021-05-13 10:50:56 -0300

    fixed bug

--- sisotool/siso_tool.py
+++ sisotool/siso_tool.py
@@ -2,12 +2,13 @@ import numpy as np
 import control as co
 import matplotlib.pyplot as plt
 
-print("==> Sisotool para python")
+print("===> Sisotool com python")
 num = (input("numerador >>")).split()
 num = [int(i) for i in num]
 den = (input("denominador >>")).split()
 den = [int(i) for i in den]
 
+# Função de transferência
 Gs = co.tf(num, den)
 
 print(Gs)
